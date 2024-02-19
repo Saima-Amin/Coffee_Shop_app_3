c4b39c8b1245036d51d426c48685b5b93a999191
Saima-Amin <saimaamin4th2022@gmail.com>
2024-02-19 08:49:11 +0600

    second commit

--- Coffee_Shop_app_3/App.js
+++ Coffee_Shop_app_3/App.js
@@ -1,12 +1,43 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {useFonts} from 'expo-font';
+import * as SplashScreen from "expo-splash-screen"
+import { useCallback } from 'react';
+
+
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
+  const [fonntsLoaded] = useFonts({
+    regular: require("./assets/fonts/OpenSans-Regular.ttf"),
+    light: require("./assets/fonts/OpenSans-Light.ttf"),
+    bold: require("./assets/fonts/OpenSans-Bold.ttf"),
+    semibold: require("./assets/fonts/OpenSans-SemiBold.ttf"),
+    extrabold: require("./assets/fonts/OpenSans-ExtraBold.ttf"),
+  })
+
+  const onLayoutRootView  = useCallback(async() => {
+    if(fonntsLoaded){
+      await SplashScreen.hideAsync();
+    }
+  },[fonntsLoaded] );
+  if(!fonntsLoaded){
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen 
+      name='Bottom Navigation'
+      component={BottomtabNavigation}
+      options={{headerShown:false}}>
+      </Stack.Screen>
+    </Stack.Navigator>
+   </NavigationContainer>
   );
 }
 
@@ -17,4 +48,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyle:{
+    fontFamily: "semibold",
+    fontSize: 20,
+  }
 });
